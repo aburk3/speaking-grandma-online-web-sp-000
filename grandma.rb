4e9b7005ce1021a0ce869d8cf18f7ea099e22839
Austin  Burke <aburk3@gmail.com>
2018-09-27 10:17:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,8 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
   end 
 end
 # Whatever you say to grandma, she should respond with
